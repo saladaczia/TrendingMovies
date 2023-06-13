6b6e4f8343ec17bf76ff02262ddf2afd260819ac
Maciej Sołoducha <biuro@goldenapp.pl>
2023-06-13 09:13:27 +0200

    Sorted files for MVVM pattern, and configure views.

---
+++ TrendingMovies/CellView/ListCell.swift
@@ -0,0 +1,31 @@
+//
+//  ListCell.swift
+//  TrendingMovies
+//
+//  Created by Maciej Sołoducha on 13/06/2023.
+//
+
+import UIKit
+
+class ListCell: UITableViewCell {
+
+    // Outlets
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var orgTitleLabel: UILabel!
+    @IBOutlet weak var posterImage: UIImageView!
+    @IBOutlet weak var yearLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var genereLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- TrendingMovies/Controller/TrendingController.swift
+++ TrendingMovies/Controller/TrendingController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TrendingController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,4 +16,5 @@ class ViewController: UIViewController {
 
 
 }
+
 
