b53127ce974117349c97441314cf56765a6e566f
Maciej Sołoducha <biuro@goldenapp.pl>
2023-06-13 08:35:49 +0200

    Initial Commit

---
+++ TrendingMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrendingMovies
+//
+//  Created by Maciej Sołoducha on 13/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
